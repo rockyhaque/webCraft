5feba895f741624a5a98cd401c5ceeabd65a28ac
Rocky Haque <rockyhaque99@gmail.com>
2023-08-06 07:11:48 +0600

    Add files via upload

---
+++ milestone_04/day_28_loop/grading.js
@@ -0,0 +1,66 @@
+var rocky = 85;
+var tom = 66;
+var jane = 95;
+var peter = 56;
+var jone = 40;
+
+// Rocky's Grade
+if(rocky >= 80)
+    console.log('Rocky Got: A');
+else if(rocky >= 60 && rocky <= 79)
+    console.log('Rocky Got: B');
+else if(rocky >= 50 && rocky <= 59)
+    console.log('Rocky Got: C');
+else if(rocky >= 40 && rocky <= 49)
+    console.log('Rocky Got: D');
+else
+    console.log('Rocky Got: F');
+
+// Tom's Grade
+if(tom >= 80)
+    console.log('Tom Got: A');
+else if(tom >= 60 && tom <= 79)
+    console.log('Tom Got: B');
+else if(tom >= 50 && tom <= 59)
+    console.log('Tom Got: C');
+else if(tom >= 40 && tom <= 49)
+    console.log('Tom Got: D');
+else
+    console.log('Tom Got: F');
+
+// Jane's Grade
+if(jane >= 80)
+    console.log('Jane Got: A');
+else if(jane >= 60 && jane <= 79)
+    console.log('Jane Got: B');
+else if(jane >= 50 && jane <= 59)
+    console.log('Jane Got: C');
+else if(jane >= 40 && jane <= 49)
+    console.log('Jane Got: D');
+else
+    console.log('Jane Got: F');
+
+// Peter's Grade
+if(peter >= 80)
+    console.log('Peter Got: A');
+else if(peter>= 60 && peter <= 79)
+    console.log('Peter Got: B');
+else if(peter >= 50 && peter <= 59)
+    console.log('Peter Got: C');
+else if(peter >= 40 && peter <= 49)
+    console.log('Peter Got: D');
+else
+    console.log('Peter Got: F');
+
+// Jone's Grade
+if(jone >= 80)
+    console.log('Jone Got: A');
+else if(jone >= 60 && jone <= 79)
+    console.log('Jone Got: B');
+else if(jone >= 50 && jone <= 59)
+    console.log('Jone Got: C');
+else if(jone >= 40 && jone <= 49)
+    console.log('Jone Got: D');
+else
+    console.log('Jone Got: F');
+
